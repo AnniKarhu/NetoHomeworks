51dcea331e12de9802a5bd51386740cfcab033ab
Annika Karhu <anna_karhu@mail.ru>
2023-09-01 00:14:17 +0300

    homework 3

---
+++ lesson-3/homework-3-1/homework-3-1.cpp
@@ -0,0 +1,140 @@
+﻿// homework-3-1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+
+
+class Calculator
+{
+private:
+    double num1 = 1;
+    double num2 = 1;
+    const std::string str_error = "Неверный ввод!\n";
+
+public:
+    const std::string str_num1 = "Введите num1: ";
+    const std::string str_num2 = "Введите num2: ";
+
+public:
+    Calculator()
+    {
+        set_num1(num1);
+        set_num2(num2);
+    }
+
+    double add() // метод должен возвращать результат сложения num1 и num2
+    {
+        return num1 + num2;
+    }
+
+    double multiply() // метод должен возвращать результат перемножения num1 и num2
+    {
+        return num1 * num2;
+    }
+
+    double subtract_1_2() // метод должен возвращать результат вычитания num2 из num1
+    {
+        return num1 - num2;
+    }
+
+    double subtract_2_1() // метод должен возвращать результат вычитания num1 из num2
+    {
+        return num2 - num1;
+    }
+
+    double divide_1_2() // метод должен возвращать результат деления num1 на num2
+    {
+        return num1 / num2;
+    }
+
+    double divide_2_1() // метод должен возвращать результат деления num2 на num1
+    {
+        return num2 / num1;
+    }
+
+    bool set_num1(double num1) // метод должен установить значение поля num1 равное значению аргумента num1 в случае, если значение аргумента не равно 0. Возвращает true, если установка произошла успешно, в обратном случае false
+    {
+        if (num1)
+        {
+            this->num1 = num1;
+            return true;
+        }
+        else
+        {
+            print_input_error();  //вывести сообщение о неверном вводе
+            return false;
+        }
+
+    }
+
+    bool set_num2(double num2) // метод должен установить значение поля num2 равное значению аргумента num2 в случае, если значение аргумента не равно 0. Возвращает true, если установка произошла успешно, в обратном случае false
+    {
+        if (num2)
+        {
+            this->num2 = num2;
+            return true;
+        }
+        else
+        {
+            print_input_error();  //вывести сообщение о неверном вводе
+            return false;
+        }
+    }
+
+    void print_input_error()  //вывести сообщение о неверном вводе
+    {
+        std::cout << str_error;
+    }
+
+    void print_calculations()
+    {
+        std::cout << "num1 + num2 = " << add() << "\n";
+        std::cout << "num1 - num2 = " << subtract_1_2() << "\n";
+        std::cout << "num2 - num1 = " << subtract_2_1() << "\n";
+        std::cout << "num1 * num2 = " << multiply() << "\n";
+        std::cout << "num1 / num2 = " << divide_1_2() << "\n";
+        std::cout << "num2 / num1 = " << divide_2_1() << "\n";
+    }
+
+};
+
+double get_num(std::string str); //ввод числа с консоли
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    double n1 = 0;
+    double n2 = 0;
+
+    Calculator calculator_1;
+
+    while (1)
+    {
+        while (!(calculator_1.set_num1(get_num(calculator_1.str_num1))))
+        {
+        };
+
+        while (!(calculator_1.set_num2(get_num(calculator_1.str_num2))))
+        {
+        };
+
+        calculator_1.print_calculations();
+
+    }
+
+};
+
+double get_num(std::string str) //ввод числа с консоли
+{
+    double num;
+    std::cout << str; // сообщение для пользователя
+    std::cin >> num;  //to-do - как проверить, что пользователь ввел число?
+    return num;
+}
+
+
+
+
+
---
+++ lesson-3/homework-3-2/homework-3-2.cpp
@@ -0,0 +1,153 @@
+﻿// homework-3-2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <Windows.h>
+
+enum class counter_command { quit = 0, plus, minus, equal };
+
+
+class Counter
+{
+private:
+    int value;
+
+
+public:
+    std::string invite_cmd_str = "Введите команду('+', '-', '=' или 'x') : ";
+
+    Counter()
+    {
+        value = 1;
+        // std::cout << "\n start value = 1\n";
+    }
+
+    Counter(int num)
+    {
+        value = num;
+        // std::cout << "\n start value = " << value << "\n";
+    }
+
+
+
+    void inc_value()
+    {
+        ++value;
+    }
+
+    void dec_value()
+    {
+        --value;
+    }
+
+    int get_value()
+    {
+        return value;
+    }
+};
+
+bool need_init_counter(); //нужно ли инициализировать значение счетчика
+int  get_start_value();   //получить начальное значение счетчика
+counter_command get_command(Counter& counter); //получить команду для счетчика от пользователя
+void start_counter(Counter& counter); //цикл с использованием счетчика
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::string yes_or_no_str = "";
+
+    Counter* p_counter;
+
+    if (need_init_counter()) //нужно ли инициализировать значение счетчика
+    {
+        p_counter = new Counter(get_start_value());   //получить начальное значение счетчика)
+          
+    }
+    else
+    {
+        p_counter = new  Counter;   //дефолтное начальное значение счетчика ( =1)       
+    }
+
+    start_counter(*p_counter);
+
+    delete p_counter;
+    return 0;
+}
+
+bool need_init_counter() //нужно ли инициализировать значение счетчика
+{
+    std::string yes_or_no_str = "";
+    while ((yes_or_no_str != "да") && (yes_or_no_str != "нет"))
+    {
+        std::cout << "Вы хотите указать начальное значение счётчика ? Введите да или нет: ";
+        std::cin >> yes_or_no_str;
+        std::cout << "\n";
+
+        if (yes_or_no_str == "да")
+            return true;
+
+        if (yes_or_no_str == "нет")
+            return false;
+    }
+}
+
+int  get_start_value()   //получить начальное значение счетчика
+{
+    std::cout << "Введите начальное значение счётчика: ";
+    int start_value;
+    std::cin >> start_value;
+
+    return start_value;
+}
+
+counter_command get_command(Counter& counter) //получить команду для счетчика от пользователя
+{
+
+    std::string cmd_str = "";
+
+    while (1)
+    {
+        std::cout << counter.invite_cmd_str;
+        std::cin >> cmd_str;
+
+        if (cmd_str == "+")
+            return counter_command::plus;
+
+        if (cmd_str == "-")
+            return counter_command::minus;
+
+        if (cmd_str == "=")
+            return counter_command::equal;
+
+        if ((cmd_str == "x") || (cmd_str == "х"))
+            return counter_command::quit;
+    }
+}
+
+void start_counter(Counter& counter) //цикл с использованием счетчика
+{
+
+    counter_command counter_cmd;
+
+    while ((counter_cmd = get_command(counter)) != counter_command::quit)
+    {
+        switch (counter_cmd)
+        {
+
+        case counter_command::plus:
+            counter.inc_value();
+            break;
+        case counter_command::minus:
+            counter.dec_value();
+            break;
+        case counter_command::equal:
+            std::cout << counter.get_value() << "\n";
+            break;
+
+        }
+    }
+    std::cout << "До свидания!\n";
+}
